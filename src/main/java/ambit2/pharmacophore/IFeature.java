980ef68cc3d3274a66ba73dfdd9703b32d9136a5
ntk73 <nick@uni-plovdiv.net>
2019-04-25 13:48:34 +0300

    Added IFeature and TestUtils

---
+++ src/main/java/ambit2/pharmacophore/IFeature.java
@@ -0,0 +1,26 @@
+package ambit2.pharmacophore;
+
+import java.util.List;
+
+import org.openscience.cdk.interfaces.IAtom;
+
+public interface IFeature 
+{
+	public static enum Type {
+		GROUP, UNDEFINED;
+		
+		public static Type fromString(String s) {
+			try {
+				Type mode = Type.valueOf(s);
+				return (mode);
+			} catch (Exception e) {
+				return Type.UNDEFINED;
+			}
+		}
+	}
+	
+	public Type getType();
+	public List<IAtom> getAtoms();
+	
+	
+}
--- src/test/java/ambit2/pharmacophore/test/TestUtils.java
+++ src/test/java/ambit2/pharmacophore/test/TestUtils.java
@@ -1,9 +1,17 @@
 package ambit2.pharmacophore.test;
 
+import org.openscience.cdk.interfaces.IAtomContainer;
+
+import ambit2.smarts.SmartsHelper;
+
 public class TestUtils {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws Exception 
+	{
+	
+		
+		IAtomContainer mol = SmartsHelper.getMoleculeFromSmiles("CCCCO");
+		System.out.println(mol.getAtomCount());
 
 	}
 
