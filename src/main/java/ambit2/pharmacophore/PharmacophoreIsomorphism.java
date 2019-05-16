6d252872c6a39ae17b28e1ef9c8355029af453a2
ntk73 <nick@uni-plovdiv.net>
2019-05-16 14:33:47 +0300

    Added  new class PharmacophoreIsomorphism

---
+++ src/main/java/ambit2/pharmacophore/PharmacophoreIsomorphism.java
@@ -0,0 +1,28 @@
+package ambit2.pharmacophore;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+
+public class PharmacophoreIsomorphism 
+{
+
+	/*
+	protected Pharpacophore query (features + connections);
+	protected IAtomContainer target + target-graph (feature instances + 3d distances);
+	
+	protected boolean isomorphismFound;
+	protected boolean FlagStoreIsomorphismNode = false;
+	protected List<Node> isomorphismNodes = new ArrayList<Node>(); 
+	protected Stack<Node> stack = new Stack<Node>();
+	
+	protected List<IFeatureInstacne> targetFI = new ArrayList<...>(); //a work container
+	protected List<FIQuerySequenceElement> sequence = new ArrayList<FIQuerySequenceElement>();
+	
+	??? protected List<IQueryAtom> sequencedAtoms = new ArrayList<IQueryAtom>();
+	??? protected List<IQueryAtom> sequencedBondAt1 = new ArrayList<IQueryAtom>();
+	??? protected List<IQueryAtom> sequencedBondAt2 = new ArrayList<IQueryAtom>();
+	
+	*/
+}
