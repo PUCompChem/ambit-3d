fd8479e0906832407bc25c743ec517ba62fd2d18
ntk73 <nick@uni-plovdiv.net>
2019-05-16 14:16:10 +0300

    Added groupMatch field

--- src/main/java/ambit2/pharmacophore/features/SmarstGroupFeature.java
+++ src/main/java/ambit2/pharmacophore/features/SmarstGroupFeature.java
@@ -1,9 +1,23 @@
 package ambit2.pharmacophore.features;
 
+import ambit2.smarts.IsomorphismTester;
+import ambit2.smarts.SmartsParser;
+import ambit2.smarts.groups.GroupMatch;
+
 public class SmarstGroupFeature implements IFeature
 {
 
 	String smarts = null;
+	private GroupMatch groupMatch = null;
+	
+	public SmarstGroupFeature(String smarts, 
+			SmartsParser parser, 
+			IsomorphismTester isoTester) throws Exception
+	{
+		this.smarts = smarts;
+		groupMatch = new GroupMatch(smarts, parser, isoTester);		
+	}	
+	
 	
 	public Type getType() {
 		return Type.SMARTS_GROUP;
