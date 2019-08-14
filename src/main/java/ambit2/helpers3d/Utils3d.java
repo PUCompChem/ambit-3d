c603bc84269e3be6a04d1420f561b81f4dc37ed8
gogohero <nrggig@gmail.com>
2019-08-14 15:44:53 +0300

    Algorithm for center of mass implemented in Utils3d

--- src/main/java/ambit2/helpers3d/Utils3d.java
+++ src/main/java/ambit2/helpers3d/Utils3d.java
@@ -10,7 +10,21 @@ public class Utils3d
 {
 	public static Point3d getAtomListCenterOfMass(List<IAtom> atoms)
 	{
-		//TODO
-		return null;
+		double x = 0.0;
+	    double y = 0.0;
+	    double z = 0.0;
+		for (int i = 0; i < atoms.size(); i++) {
+			Point3d point = atoms.get(i).getPoint3d();
+			 x += point.getX();
+			 y += point.getY();
+			 z += point.getZ();
+		}
+		
+		double avrX = x/atoms.size();
+		double avrY = y/atoms.size();
+		double avrZ = z/atoms.size();
+		
+		Point3d point = new Point3d(avrX,avrY,avrZ);
+		return point;
 	}
 }
