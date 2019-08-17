068e3692188f3f9e49c459125a89b5df48f54d83
ntk73 <nick@uni-plovdiv.net>
2019-08-17 09:38:45 +0300

    Implemented function getCEnterOfMass()

--- src/main/java/ambit2/helpers3d/Utils3d.java
+++ src/main/java/ambit2/helpers3d/Utils3d.java
@@ -8,23 +8,51 @@ import org.openscience.cdk.interfaces.IAtom;
 
 public class Utils3d 
 {
-	public static Point3d getAtomListCenterOfMass(List<IAtom> atoms)
+	public static Point3d getCenterOfMass(List<IAtom> atoms)
 	{
 		double x = 0.0;
 	    double y = 0.0;
 	    double z = 0.0;
-		for (int i = 0; i < atoms.size(); i++) {
-			Point3d point = atoms.get(i).getPoint3d();
-			 x += point.getX();
-			 y += point.getY();
-			 z += point.getZ();
+	    double M = 0.0;
+	    
+		for (IAtom at : atoms) {
+			Point3d point = at.getPoint3d();
+			double m = at.getExactMass();
+			M += m;
+			x += point.getX()/m;
+			y += point.getY()/m;
+			z += point.getZ()/m;
 		}
 		
-		double avrX = x/atoms.size();
-		double avrY = y/atoms.size();
-		double avrZ = z/atoms.size();
+		x = x/M;
+		y = y/M;
+		z = z/M;
 		
-		Point3d point = new Point3d(avrX,avrY,avrZ);
+		Point3d point = new Point3d(x,y,z);
 		return point;
 	}
+	
+	
+	public static Point3d getAvergageCoordinate(List<IAtom> atoms)
+	{
+		double x = 0.0;
+	    double y = 0.0;
+	    double z = 0.0;
+	    
+		for (int i = 0; i < atoms.size(); i++) {
+			Point3d point = atoms.get(i).getPoint3d();
+			x += point.getX();
+			y += point.getY();
+			z += point.getZ();
+		}
+		
+		x = x/atoms.size();
+		y = y/atoms.size();
+		z = z/atoms.size();
+		
+		Point3d point = new Point3d(x,y,z);
+		return point;
+	}
+	
+	
 }
