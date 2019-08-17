b39535dad5d195a083dc86e661df51f2ad4a234f
ntk73 <nick@uni-plovdiv.net>
2019-08-17 09:21:51 +0300

    Added new class FeatureUtils

---
+++ src/main/java/ambit2/pharmacophore/features/FeatureUtils.java
@@ -0,0 +1,15 @@
+package ambit2.pharmacophore.features;
+
+import javax.vecmath.Point3d;
+
+public class FeatureUtils 
+{
+	public static Point3d calcFeatuteCoordinates(FeatureInstance finstance)
+	{
+		switch (finstance.feature.getFeatureCoordinatesAlgorithm())
+		{
+		//TODO
+		}
+		return null;
+	}
+}
