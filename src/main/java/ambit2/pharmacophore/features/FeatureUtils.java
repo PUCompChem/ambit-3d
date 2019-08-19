ebc73ea0c5a221a6c85ecdd88e930cd4cf126eda
gogohero <nrggig@gmail.com>
2019-08-19 12:34:34 +0300

    FeatureUtils calculate coordinates cases added

--- src/main/java/ambit2/pharmacophore/features/FeatureUtils.java
+++ src/main/java/ambit2/pharmacophore/features/FeatureUtils.java
@@ -2,14 +2,22 @@ package ambit2.pharmacophore.features;
 
 import javax.vecmath.Point3d;
 
+import ambit2.helpers3d.Utils3d;
+
 public class FeatureUtils 
 {
 	public static Point3d calcFeatuteCoordinates(FeatureInstance finstance)
 	{
 		switch (finstance.feature.getFeatureCoordinatesAlgorithm())
 		{
-		//TODO
+		
+			case MASS_CENTER:
+				return Utils3d.getCenterOfMass(finstance.atoms);
+		 
+			case AVERAGE:
+				return	Utils3d.getAvergageCoordinate(finstance.atoms);
+			
+			default: return null;
 		}
-		return null;
 	}
 }
